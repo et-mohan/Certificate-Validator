30cc26a7da27779c8d31eca37195eb76cf7f51ad
Mohankumar <91587795+et-mohan@users.noreply.github.com>
2025-02-03 10:48:36 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/env bash
 # Install Tesseract OCR
-apt-get update && apt-get install -y tesseract-ocr
+wget https://github.com/tesseract-ocr/tesseract/archive/refs/tags/4.1.1.tar.gz
+tar -zxvf 4.1.1.tar.gz
+cd tesseract-4.1.1
+./autogen.sh
+./configure
+make
+make install
+ldconfig
+cd ..
+rm -rf tesseract-4.1.1 4.1.1.tar.gz
